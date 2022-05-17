18a4f4322a4aa669ce4c78df24dfebd1481abdc3
Andy <andy@itfactory.ro>
2022-05-17 17:10:31 +0300

    Add files via upload

---
+++ login_test.py
@@ -0,0 +1,19 @@
+# avem nevoie sa importam cateva librarii gratuite care ne ajuta sa controlam chrome
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+
+# initializam chrome
+s = Service(ChromeDriverManager().install())
+chrome = webdriver.Chrome(service=s)
+
+# maximizam fereastra
+chrome.maximize_window()
+
+# navigam catre un url
+chrome.get('https://the-internet.herokuapp.com/login')
+
+# cu sleep putem pune pauza de cateva secunde sa asteptam sa vedem si noi ceva
+sleep(3)
