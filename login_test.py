ad90818c19b89af85881d225dc98a2c68c8c0188
Andy <andy@itfactory.ro>
2022-10-22 12:35:21 +0300

    Update login_test.py

--- login_test.py
+++ login_test.py
@@ -17,3 +17,6 @@ chrome.get('https://the-internet.herokuapp.com/login')
 
 # cu sleep putem pune pauza de cateva secunde sa asteptam sa vedem si noi ceva
 sleep(3)
+
+# inchidem chrome
+chrome.quit()
