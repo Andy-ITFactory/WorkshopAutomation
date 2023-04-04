6e6123ee73ffc042e065b488b68f067f0f991b12
Andy <andy@itfactory.ro>
2023-04-04 13:47:11 +0300

    Update start_test.py

--- start_test.py
+++ start_test.py
@@ -6,30 +6,48 @@ from time import sleep
 # initializam chrome
 chrome = webdriver.Chrome()
 
-# maximizam fereastra
-chrome.maximize_window()
-
 # navigam catre un url
 chrome.get('https://formy-project.herokuapp.com/form')
 
 # cu sleep putem pune pauza de cateva secunde sa asteptam sa vedem si noi ceva
 sleep(3)
 
+# exercitiu - maximizam fereastra
+# toate comenzile incep cu chrome. si sunt intuitive
+# scrie mai jos chrome. si gandeste-te care ar putea fi comanda corecta
+
+
 # gasim first name dupa ID si scriem valori in formular
 chrome.find_element(By.ID, 'first-name').send_keys("Andy")
 sleep(3)
 
-# vom completa la workshop last name si job title
+# exercitiu: completam impreuna nume apoi punem un sleep de 3 secunde
 
-# gasim butonul de Submit dupa textul de pe link si dam click pe el
-chrome.find_element(By.LINK_TEXT, 'Submit').click()
-sleep(3)
 
-# gasim titlul si verificam mesajul
-thank_you_msg = chrome.find_element(By.XPATH, '/html/body/div/h1').text
-assert thank_you_msg == 'Thanks for submitting your form'
+# exercitiu: completeaza singur job title
+# nu uitam de sleep
 
-# verificati voi mesajul de mai jos "The form was successfully submitted!"
+
+# exercitiu: select education - nu mai folosim .send_keys() la final. ce metoda crezi ca ar merge aici?
+# nu uitam de sleep
+
+
+# exercitiu: select gender
+# nu uitam de sleep
+
+
+# exercitiu: dam click pe submit apoi punem un sleep de 3 secunde
+# vom invata ce sa facem cand elementul nu are ID
+
+
+# inchidem chrome
+chrome.quit()
 
 # daca a trecut testul, printam in consola un mesaj de succes
 print('SUCCESS - TEST PASSED')
+
+# exercitiu final:
+# stergem toate comenzile de sleep pentru a vedea viteza testului in timp real
+# aceasta EFICIENTA ofera o valoare ENORMA si PERMANENTA angajatorilor
+# costul acestui test tinde spre 0, o data ce a fost scris
+# asa ca ei vor plati FOARTE BINE o data, ca sa primeasca si sa ramana cu aceasta valoare care ulterior, tinde spre ∞
