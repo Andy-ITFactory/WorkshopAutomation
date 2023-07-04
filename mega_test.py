aec0bc19383a9a3627852cad574fc2d62ff38013
Andy <andy@itfactory.ro>
2023-07-04 14:48:44 +0300

    Update mega_test.py

--- mega_test.py
+++ mega_test.py
@@ -9,21 +9,24 @@ chrome = webdriver.Chrome()
 # cu sleep putem pune pauza de cateva secunde sa asteptam sa vedem si noi ceva
 sleep(3)
 
-# maximizam fereastra
+# maximizam fereastra (Aici incercati sa gasiti singuri comanda)
 
-# navigam catre un url (mega image home page)
+# navigam catre un url: "https://www.mega-image.ro/" (Aici incercati sa gasiti singuri comanda)
 
-# dam click pe Accept cookies
+# dam click pe Accept cookies (Aici explica host)
 # chrome.find_element(By.XPATH, '//*[@data-testid="de_gasit_si_completat_id_corect"]').click()
 
-# dam click pe Contul meu
+# dam click pe Contul meu (Aici recapitulam impreuna)
 
-# dam click pe Continua
+# dam click pe Continua (Aici faceti singuri sa verificati daca ati inteles)
 
-# verificam mesajul de eroare "Te rugam sa introduci adresa de e-mail sau numarul de telefon"
+# verificam mesajul de eroare "Te rugam sa introduci adresa de e-mail sau numarul de telefon" (Explica host)
 
 # inchidem chrome
 chrome.quit()
 
 # daca a trecut testul, printam in consola un mesaj de succes
 print('SUCCESS - TEST PASSED')
+
+# la final scoatem sleep-urile pentru a vedea viteza unui test si pentru a intelege valoarea si necesitatea unui automation engineer
+# la final va aratam si un exemplu de raport generat automat
